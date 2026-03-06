5e20098146b63f8f382f0b9d5bd9f4ac1ebf16b4
LorisPrataiolo <lorisprataiolo.universita@gmail.com>
2026-03-06 17:23:45 +0100

    first commit

---
+++ custom-encrypted-protocol-and-sniffer/src/client.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/errno.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+
+
+void fail_errno(const char * const  );
+void fail (const char * const  );
+void check_main_arg (int , char  *[]);
+int parse_ipv4addr(char *, struct in_addr *);
+int parse_port(const char *);
+
+
+/* ================================================================== */
+
+int main(int argc, char  *argv[]) {
+
+    check_main_arg(argc, argv);
+
+    struct in_addr server_addr;
+    if (parse_ipv4addr(argv[1], &server_addr))
+        printf ("Server IP address: valid\n");
+
+    int port =  parse_port(argv[2]);
+    printf("Port: valid\n");
+
+
+return 0;
+}
+
+
+
+/* ================================================================== */
+void fail_errno(const char * const msg ) {
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
+
+void fail(const char * const msg) {
+    fprintf(stderr, "%s\n",msg);
+    exit(EXIT_FAILURE);
+}
+
+void check_main_arg (int argc, char  *argv[]){
+    if (argc != 3) 
+        fail("Usage: ./client <IPv4 address> <number port>");
+}
+
+int parse_ipv4addr(char*ip, struct in_addr *addr){
+    int res = inet_pton(AF_INET, ip, &addr);
+
+    if (res == 0) 
+        fail("Invalid IPv4 address");
+    
+    if (res ==  - 1)
+        fail_errno("Invalid IPv4 address");
+
+  return res;
+}
+
+int parse_port(const char *port_str)
+{
+    char *endptr;
+    errno = 0;
+
+    long port = strtol(port_str, &endptr, 10);
+
+    if (errno != 0)
+        fail_errno("strtol");
+
+    if (*endptr != '\0')
+        fail("Port must be numeric");
+
+    if (port < 1 || port > 65535)
+        fail("Port must be between 1 and 65535");
+
+    return (int)port;
+}
+
